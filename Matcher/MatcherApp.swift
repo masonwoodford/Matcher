22a9bbb6bf1cb657a629b45a5ac813526cb7427c
Mason Woodford <63433735+masonwoodford@users.noreply.github.com>
2022-03-05 21:17:20 -0800

    Initial Commit

---
+++ Matcher/MatcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatcherApp.swift
+//  Matcher
+//
+//  Created by Mason Woodford on 3/5/22.
+//
+
+import SwiftUI
+
+@main
+struct MatcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
